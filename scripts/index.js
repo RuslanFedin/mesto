b1d01d7ffa447efc23d21697d40601c856309908
Ruslan Fedin <fedin.ra@mail.ru>
2022-03-21 23:35:08 +0300

    Скоррестированы стили и разметка по замечаниям

--- scripts/index.js
+++ scripts/index.js
@@ -6,24 +6,24 @@ const ESC_KEY = "Escape"; // Объявляем клавишу Esc
 let formElement = document.querySelector('.popup__container'); // Запрашиваем форму попапа
 let nameInput = formElement.querySelector('.popup__name'); // Запрашиваем строку ввода имени
 let professionInput = formElement.querySelector('.popup__profession'); // Запрашиваем строку ввода професии
-let Name = document.querySelector('.profile__name'); // Находим  элемент с изменяемым текстом
-let Profession = document.querySelector('.profile__profession'); //Находим  элемент с изменяемым текстом
+let name = document.querySelector('.profile__name'); // Находим  элемент с изменяемым текстом
+let profession = document.querySelector('.profile__profession'); //Находим  элемент с изменяемым текстом
 
-function OpenPopup () {
+function openPopup () {
   popupElement.classList.add('popup_opened');
   document.addEventListener('keyup', onDocumentKeyUp);
-  nameInput.value = Name.textContent;
-  professionInput.value = Profession.textContent;
+  nameInput.value = name.textContent;
+  professionInput.value = profession.textContent;
 }
 
-function ClosePopup () {
+function closePopup () {
   popupElement.classList.remove('popup_opened');
   document.removeEventListener('keyup', onDocumentKeyUp);
 }
 
 function onDocumentKeyUp(event){
   if (event.key === ESC_KEY){
-    ClosePopup();
+    closePopup();
   }
 }
 
@@ -31,11 +31,11 @@ function formSubmitHandler (evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
                         // Так мы можем определить свою логику отправки.
                         // О том, как это делать, расскажем позже.
-  Name.textContent = nameInput.value; // Вставляем текстовое содержимое из value в элемент.
-  Profession.textContent = professionInput.value; // Вставляем текстовое содержимое из value в элемент.
-  ClosePopup();
+  name.textContent = nameInput.value; // Вставляем текстовое содержимое из value в элемент.
+  profession.textContent = professionInput.value; // Вставляем текстовое содержимое из value в элемент.
+  closePopup();
 }
 
-editButton.addEventListener('click', OpenPopup); // Обработчик событий на кнопке редактирования профиля.
-closeButton.addEventListener('click', ClosePopup); // Обработчик событий на кнопке закрытия формы.
+editButton.addEventListener('click', openPopup); // Обработчик событий на кнопке редактирования профиля.
+closeButton.addEventListener('click', closePopup); // Обработчик событий на кнопке закрытия формы.
 formElement.addEventListener('submit', formSubmitHandler); // Обработчик событий в форме попапа: он будет следить за событием “submit” - «отправка»
